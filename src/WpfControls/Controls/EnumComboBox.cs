630d6552a640312b9ceadfd3824b0217bfeb8a88
Ralf Beckers <ralf.beckers@elektrobit.com>
2024-08-08 12:04:35 +0200

    Add EnumComboBox Image handling

--- src/WpfControls/Controls/EnumComboBox.cs
+++ src/WpfControls/Controls/EnumComboBox.cs
@@ -1,13 +1,32 @@
-﻿using WpfControls.Internal;
-
-namespace WpfControls.Controls;
+﻿namespace WpfControls.Controls;
 
 public class EnumComboBox : ComboBox
 {
     public EnumComboBox()
     {
-        DisplayMemberPath = "Description";
-        SelectedValuePath = "Value"; 
+        //DisplayMemberPath = "Description";
+        SelectedValuePath = "Value";
+
+        //var template = new DataTemplate() { DataType = typeof(string) };
+        //var dockPanel = new FrameworkElementFactory(typeof(DockPanel));
+        //dockPanel.SetValue(DockPanel.HorizontalAlignmentProperty, HorizontalAlignment.Stretch);
+
+        //var image = new FrameworkElementFactory(typeof(Image));
+        //image.SetBinding(Image.SourceProperty, new Binding("Image"));
+        //image.SetValue(Image.WidthProperty, 16.0);
+        //image.SetValue(Image.HeightProperty, 16.0);
+        //image.SetValue(Image.MarginProperty, new Thickness(0, 0, 4.0, 0.0));
+
+        //var textBlock = new FrameworkElementFactory(typeof(TextBlock));
+        //textBlock.SetBinding(TextBlock.TextProperty, new Binding("Description"));
+        //textBlock.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
+        //textBlock.SetValue(TextBlock.BackgroundProperty, Brushes.Yellow);
+
+        //dockPanel.AppendChild(image);
+        //dockPanel.AppendChild(textBlock);
+        //template.VisualTree = dockPanel;
+
+        //this.ItemTemplate = template;
     }
 
     public static readonly DependencyProperty EnumTypeProperty =
@@ -27,34 +46,72 @@ public class EnumComboBox : ComboBox
         {
             throw new ArgumentException("Type must be an Enum.");
         }
-        this.ItemsSource = Enum.GetValues(enumType).Cast<object>().Select(i => new EnumerationMember(i, GetString(i))).ToList();
-    }
 
-    private static string GetString(object item)
-    {
-        FieldInfo fieldInfo = item.GetType().GetField(item.ToString()!)!;
-        if (fieldInfo.GetCustomAttribute<ResourceAttribute>() is ResourceAttribute resourceAttribute)
+        var enumValues = Enum.GetValues(enumType).Cast<object>();
+
+        // check if any enum value has the ImageAttribute
+        if (enumValues.Any(item => item.GetFieldInfo().GetCustomAttribute<ImageAttribute>() is not null))
         {
-            return EntryAssemblyResourceManager.GetString(resourceAttribute.Name) ?? "";
-        }
-        else if (fieldInfo.GetCustomAttribute<DescriptionAttribute>() is DescriptionAttribute descriptionAttribute)
-        {
-            return descriptionAttribute.Description;
+            // Create ItemTemplate for Images and Text
+            var template = new DataTemplate() { DataType = typeof(string) };
+            var dockPanel = new FrameworkElementFactory(typeof(DockPanel));
+            dockPanel.SetValue(DockPanel.HorizontalAlignmentProperty, HorizontalAlignment.Stretch);
+
+            var image = new FrameworkElementFactory(typeof(Image));
+            image.SetBinding(Image.SourceProperty, new Binding("Image"));
+            image.SetValue(Image.WidthProperty, 16.0);
+            image.SetValue(Image.HeightProperty, 16.0);
+            image.SetValue(Image.MarginProperty, new Thickness(0, 0, 4.0, 0.0));
+
+            var textBlock = new FrameworkElementFactory(typeof(TextBlock));
+            textBlock.SetBinding(TextBlock.TextProperty, new Binding("Description"));
+            textBlock.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
+            textBlock.SetValue(TextBlock.BackgroundProperty, Brushes.Yellow);
+
+            dockPanel.AppendChild(image);
+            dockPanel.AppendChild(textBlock);
+            template.VisualTree = dockPanel;
+
+            this.ItemTemplate = template;
         }
         else
         {
-            return fieldInfo.Name;
+            DisplayMemberPath = "Description";
         }
+
+        this.ItemsSource = enumValues.Select(i => new EnumerationMember(i)).ToList();
     }
 
-    protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+    public class EnumerationMember 
     {
-        base.OnSelectionChanged(e);
-    }
+        public EnumerationMember(object item)
+        {
+            Value = item;
 
-    public class EnumerationMember(object value, string description)
-    {
-        public string Description { get; } = description;
-        public object Value { get; } = value;
+            FieldInfo fieldInfo = item.GetFieldInfo();
+            if (fieldInfo.GetCustomAttribute<DescriptionAttribute>() is DescriptionAttribute descriptionAttribute)
+            {
+                Description = descriptionAttribute.Description;
+            }
+            else if (fieldInfo.GetCustomAttribute<ResourceAttribute>() is ResourceAttribute resourceAttribute)
+            {
+                Description = EntryAssemblyResourceManager.GetString(resourceAttribute.Name) ?? "";
+            }            
+            else
+            {
+                Description = fieldInfo.Name;
+            }
+
+            if (fieldInfo.GetCustomAttribute<ImageAttribute>() is ImageAttribute imageAttribute)
+            {
+                Image = imageAttribute.Source;
+            }
+        }
+
+        public object Value { get; }
+
+        public string Description { get; }
+
+        public string Image { get; } = string.Empty;
     }
 }
